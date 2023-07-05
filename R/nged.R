bf2fec332f64fde13e3340ea10a289fbddbef502
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-07-05 20:24:48 +0330

    add matrix

--- R/nged.R
+++ R/nged.R
@@ -3,11 +3,11 @@
 #' Computes the pdf, cdf, hdf, quantile and random numbers of the
 #'
 #' @name NGED
-#' @param x scaler or vector of values at which the pdf or cdf needs to be computed.
+#' @param x scaler or vector or matrix lower and upper of values at which the pdf or cdf needs to be computed.
 #' @param q scaler or vector of probabilities at which the quantile needs to be computed.
 #' @param n number of random numbers to be generated.
-#' @param v the value of the first shape parameter.
-#' @param delta the value of the second shape parameter, must be positive, the default is 2.
+#' @param v the value or vector lower and upper of the first shape parameter.
+#' @param delta the value or vector lower and upper of the second shape parameter, must be positive, the default is 2.
 #'
 #' @return  \code{pnged} gives the distribution function,
 #'  \code{dnged} gives the density,
@@ -23,10 +23,21 @@
 #' x2 <- matrix(seq(0, 2, length.out = 40),ncol=2)
 #' pnged(x)
 #' pnged(x, v = 2, delta = 1)
+#' pnged(x2, v = c(1,2), delta = c(2,2))
 #' @export
 
 pnged <- function(x, v=1, delta=2) {
-  F0 <- (1 - exp(-x / v))^delta
+  if (is.vector(x)){
+    F0 <- (1 - exp(-x / v[1]))^delta[1]
+  }else{
+    if(length(v)<2||length(delta)<2){
+      stop(message = "incompatible arguments.")
+    }else{
+      F0 <- matrix(data=NA,nrow=nrow(x),ncol=2)
+      F0[,1] <- (1 - exp(-x[,1] / v[1]))^delta[1]
+      F0[,2] <- (1 - exp(-x[,2] / v[2]))^delta[2]
+    }
+  }
   return(F0)
 }
 
@@ -34,38 +45,60 @@ pnged <- function(x, v=1, delta=2) {
 #' @examples
 #' dnged(x, v = 1, delta = 2)
 #' curve(dnged, -3, 3)
+#' dnged(x2, v = c(1,2), delta = c(2,2))
 #' @export
-
 dnged <- function(x, v = 1, delta = 2) {
-  df <- (delta / v) * (1 - exp(-x / v))^(delta - 1) * exp(-x / v)
+ if (is.vector(x)){
+    df <- (delta[1] / v[1]) * (1 - exp(-x / v[1]))^(delta[1] - 1) * exp(-x / v[1])
+  }else{
+    if(length(v)<2||length(delta)<2){
+      stop(message = "incompatible arguments.")
+    }else{
+      df <- matrix(data=NA,nrow=nrow(x),ncol=2)
+      df[,1] <- (delta[1] / v[1]) * (1 - exp(-x[,1] / v[1]))^(delta[1] - 1) * exp(-x[,1] / v[1])
+      df[,2] <- (delta[2] / v[2]) * (1 - exp(-x[,2] / v[2]))^(delta[2] - 1) * exp(-x[,2] / v[2])
+    }
+  }
   return(df)
 }
 
 #' @name NGED
 #' @examples
 #' qnged(x, v = 1, delta = 3)
+#' x2 <- matrix(seq(0, 1, length.out = 40),ncol=2)
+#' qnged(x, v = c(1,2), delta = c(2,2))
 #' @export
-
 qnged <- function(q, v = 1, delta = 2) {
-  q0 <- function(x0) {
-    if (x0 < 0 || x0 > 1) stop(message = "[Warning] 0 < x < 1.")
-    F0 <- function(t) x0 - pnged(t, v, delta)
-    F0 <- Vectorize(F0)
-    x0 <- uniroot(F0, interval = c(0, 1e+15))$root
-    return(x0)
+  if (any(q < 0) || any(q > 1)) stop(message = "[Warning] 0 < x < 1.")
+  if (is.vector(x)){
+    qf <- log(-q^(1 / delta[1]) + 1) * (-v[1])
+  }else{
+    if(length(v)<2||length(delta)<2){
+      stop(message = "incompatible arguments.")
+    }else{
+      qf <- matrix(data=NA,nrow=nrow(x),ncol=2)
+      qf[,1] <- log(-q[,1]^(1 / delta[1]) + 1) * (-v[1])
+      qf[,2] <- log(-q[,2]^(1 / delta[2]) + 1) * (-v[2])
+    }
   }
-  return(sapply(q, q0))
 }
-
 
 #' @name NGED
 #' @examples
 #' n <- 10
 #' rnged(n, v = 2, delta=1)
+#' rnged(n, v = c(2,2), delta=c(1,2))
 #' @export
 rnged <- function(n, v = 1, delta = 2) {
-  u <- runif(n)
-  X <- log(-u^(1 / delta) + 1) * (-v)
+  if(length(v)<2||length(delta)<2){
+    u <- runif(n)
+    X <- log(-u^(1 / delta[1]) + 1) * (-v[1])
+    }else{
+    u <- matrix(runif(n*2),nrow=n,ncol=2)
+    X <- matrix(data=NA,nrow=n,ncol=2)
+    X[,1] <- log(-u[,1]^(1 / delta[1]) + 1) * (-v[1])
+    X[,2] <- log(-u[,2]^(1 / delta[2]) + 1) * (-v[2])
+    }
   return(X)
 }
 
@@ -74,10 +107,21 @@ rnged <- function(n, v = 1, delta = 2) {
 #' @examples
 #' hnged(x, v = 1, delta = 3)
 #' curve(hnged, 0, 3)
+#' hnged(x2, v = c(1,2), delta = c(2,2))
 #' @export
 
 hnged <- function(x, v = 1, delta = 2) {
-  h <- ((delta / v) * (1 - exp(-x / v))^(delta - 1) * exp(-x / v)) / (1 - (1 - exp(-x / v))^delta)
+  if (is.vector(x)){
+    h <- ((delta[1] / v[1]) * (1 - exp(-x / v[1]))^(delta[1] - 1) * exp(-x / v[1])) / (1 - (1 - exp(-x / v[1]))^delta[1])
+  }else{
+    if(length(v)<2||length(delta)<2){
+      stop(message = "incompatible arguments.")
+    }else{
+      h <- matrix(data=NA,nrow=nrow(x),ncol=2)
+      h[,1] <- ((delta[1] / v[1]) * (1 - exp(-x[,1] / v[1]))^(delta[1] - 1) * exp(-x[,1] / v[1])) / (1 - (1 - exp(-x[,1] / v[1]))^delta[1])
+      h[,2] <- ((delta[2] / v[2]) * (1 - exp(-x[,2] / v[2]))^(delta[2] - 1) * exp(-x[,2] / v[2])) / (1 - (1 - exp(-x[,2] / v[2]))^delta[2])
+    }
+  }
   return(h)
 }
 
@@ -99,7 +143,6 @@ snged <- function(x, v = 1, delta = 2) {
       s[,2] <- 1 - (1 - exp(-x[,2] / v[2]))^delta[2]
     }
   }
-
   return(s)
 }
 
