a2f637015f75e03e92d4fb60e0425a6d4dd5c3c9
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2023-08-08 19:43:39 +0330

    new

---
+++ R/nbind.R
@@ -0,0 +1,141 @@
+#' Neutrosophic Binomial Distribution (NBIND)
+#'
+#' Density, distribution function, quantile function and random
+#' generation for the nuetrosophic Binomial distribution with
+#' parameters \eqn{n} and \eqn{p_N}.
+#'
+#' The neutrosophic Poisson distribution with parameter \eqn{\lambda_N}
+#' has the density
+#' \deqn{f_X(x)=\bigg(\begin{array}{c}n \\ x\end{array}\bigg) p_N^{x}\left(1-p_N\right)^{n-x},  x=0, 1, 2, \ldots, n}
+#' for \eqn{p_N \in (pL, \lambda_U)} which must be \eqn{0<p_N<1}
+#' interval and \eqn{n \in \{0, 1, 2, \ldots\}} and \eqn{x \in \{0, 1, 2, \ldots\}}.
+#'
+#' @name NBIND
+#' @param x a vector or matrix of observations for which the pdf needs to be computed.
+#' @param q a vector or matrix of quantiles for which the cdf needs to be computed.
+#' @param p a vector or matrix of probabilities for which the quantile needs to be computed.
+#' @param n number of random values to be generated.
+#' @param size number of trials (zero or more), which must be a positive interval.
+#' @param prob probability of success on each trial.
+#' @param lower.tail logical; if TRUE (default), probabilities are
+#' \eqn{P(X \ge x)}; otherwise, \eqn{P(X >x)}.
+#'
+#' @return
+#'  \code{pbind} gives the distribution function,
+#'  \code{dbind} gives the density,
+#'  \code{qbind} gives the quantile function and
+#'  \code{rbind} generates random variables from the neutrosophic Poisson Distribution.
+#' @references
+#'        Granados, C. (2022).
+#'        Some discrete neutrosophic distributions with neutrosophic parameters based on neutrosophic random variables.
+#'         \emph{Hacettepe Journal of Mathematics and Statistics}, 51(5), 1442-1457.
+#' @importFrom stats runif dpois ppois qpois
+#' @examples
+#' dbind(x, size = 2, prob= 0.5)
+#' dbind(x2, lambda = c(2, 2))
+#' @export
+dbind <- function(x, size, prob) {
+    if (any(size <= 0) || any(prob <= 0) || any(prob > 1) || any(x < 0))
+    stop(message = "Arguments are incompatible.")
+
+  if (any(x < 0) && any(x - floor(x) == 0))
+    stop(message = "Warning: x should be a positive integer.")
+
+  size <- rep(size, length.out = 2)
+  prob  <- rep(prob, length.out = 2)
+
+  if(is.vector(x)){
+    x <- matrix(rep(x, length.out = 2), ncol = 2)
+  }
+
+  x <- matrix(x, ncol = 2)
+
+  pdf <- matrix(data = NA, nrow = nrow(x), ncol = ncol(x))
+  for (i in 1:ncol(x)) {
+    pdf[, i] <- stats::dbinom(x[, i], size = size[i], prob = prob[i])
+  }
+
+  swap_rows <- pdf[, 1] > pdf[, 2]
+  pdf[swap_rows, c(1, 2)] <- pdf[swap_rows, c(2, 1)]
+
+  return(pdf)
+}
+#' @name NBIND
+#' @examples
+#' x <- 1:10
+#' x2 <- matrix(1:20, ncol = 2)
+#' pbind(x, size = 2, prob= 0.5)
+#' pbind(x2, size = c(2,2), prob = c(.3 ,.6))
+#' @export
+
+pbind <- function(q, size, prob, lower.tail = TRUE) {
+  if (any(size <= 0) || any(prob <= 0) || any(prob > 1) || any(q < 0))
+    stop(message = "Arguments are incompatible.")
+  if (any(q < 0) && any(q - floor(q) == 0))
+    stop(message = "Warning: q should be a  positive integer.")
+
+  size <- rep(size, length.out = 2)
+  prob  <- rep(prob, length.out = 2)
+  if (is.vector(q)){
+    q <- rep(q, length.out = 2)
+  }
+  q <- matrix(q, ncol = 2)
+
+  cdf <- stats::dbinom(q, size = size, prob = prob)
+
+  if (!lower.tail)
+    cdf <- 1 - cdf
+
+  cdf <- matrix(cdf, ncol = 2, byrow = TRUE)
+
+  swap_rows <- cdf[, 1] > cdf[, 2]
+  cdf[swap_rows, c(1, 2)] <- cdf[swap_rows, c(2, 1)]
+
+  return(cdf)
+}
+#' @name NBIND
+#' @examples
+#' q1 <- seq(0.1, 1, length.out = 40)
+#' qbind(q1, size = 2, prob= 0.5)
+#' q2 <- matrix(seq(0.1, 1, length.out = 40), ncol = 2)
+#' qbind(q2, lambda = c(2, 2))
+#' @export
+qbind <- function(p, size, prob) {
+  if (any(p < 0) || any(p > 1))
+    stop(message = "Warning: p should be in the interval [0,1].")
+
+  if (any(lambda < 0))
+    stop(message = "Arguments are incompatible.")
+
+  size <- rep(size, length.out = 2)
+  prob  <- rep(prob, length.out = 2)
+  p <- matrix(rep(p, each = 2), ncol = 2, byrow = TRUE)
+
+  quantiles <- matrix(data = NA, nrow = nrow(p), ncol = 2)
+  for (i in 1:ncol(p)) {
+    quantiles[, i] <- stats::qbinom(p[, i], size = size[i], prob = prob[i])
+  }
+
+  swap_rows <- quantiles[, 1] > quantiles[, 2]
+  quantiles[swap_rows, c(1, 2)] <- quantiles[swap_rows, c(2, 1)]
+
+  return(quantiles)
+}
+
+#' @name NBIND
+#' @examples
+#' n <- 10
+#' rbind(n, size = 2, prob= 0.5)
+#' rbind(n, lambda = c(1, 2))
+#' @export
+rbind <- function(n, size, prob) {
+  if (any(lambda < 0))
+    stop(message = "Arguments are incompatible.")
+
+  size <- rep(size, length.out = 2)
+  prob  <- rep(prob, length.out = 2)
+  u <- matrix(runif(n), ncol = 2)
+  X <- qbind(u, lambda)
+
+  return(X)
+}
