239fb4dc29cac2bdc4fb339587d865c1618570cd
Mina Norouzirad <86372230+mnrzrad@users.noreply.github.com>
2023-08-19 23:34:08 +0100

    Update nnbinom.R

--- R/nnbinom.R
+++ R/nnbinom.R
@@ -10,14 +10,13 @@
 #' for \eqn{r_N \in \{1, 2, \ldots\}} and \eqn{p_N \in (p_L, p_U)} which must be \eqn{0<p_N<1}
 #' and \eqn{x \in \{0, 1, 2, \ldots\}}.
 #'
-#' @name NNBIND
+#' @name Neutrosophic Negative Binomial
 #' @param x a vector or matrix of observations for which the pdf needs to be computed.
 #' @param q a vector or matrix of quantiles for which the cdf needs to be computed.
 #' @param p a vector or matrix of probabilities for which the quantile needs to be computed.
 #' @param n number of random values to be generated.
 #' @param size number of trials (zero or more), which must be a positive interval.
 #' @param prob probability of success on each trial, \eqn{0 < prob <= 1}.
-#' @param log,log.p logical; if TRUE, probabilities p are given as log(p).
 #' @param lower.tail logical; if TRUE (default), probabilities are
 #' \eqn{P(X \ge x)}; otherwise, \eqn{P(X >x)}.
 #'
@@ -35,7 +34,7 @@
 #' dnnbinom(x, size = 2, prob= 0.5)
 #' dnnbinom(x2, lambda = c(2, 2))
 #' @export
-dnnbinom <- function(x, size, prob, log = FALSE) {
+dnnbinom <- function(x, size, prob) {
   if (any(size < 1) || any(prob <= 0) || any(prob > 1) || any(x < 0))
     stop(message = "Arguments are incompatible.")
 
@@ -53,7 +52,7 @@ dnnbinom <- function(x, size, prob, log = FALSE) {
 
   pdf <- matrix(data = NA, nrow = nrow(x), ncol = ncol(x))
   for (i in 1:ncol(x)) {
-    pdf[, i] <- stats::dnbinom(x[, i], size = size[i], prob = prob[i], log = log)
+    pdf[, i] <- stats::dnbinom(x[, i], size = size[i], prob = prob[i])
   }
 
   swap_rows <- pdf[, 1] > pdf[, 2]
@@ -61,7 +60,7 @@ dnnbinom <- function(x, size, prob, log = FALSE) {
 
   return(pdf)
 }
-#' @name NNBIND
+#' @name Neutrosophic Negative Binomial
 #' @examples
 #' x <- 1:10
 #' x2 <- matrix(1:20, ncol = 2)
@@ -69,7 +68,7 @@ dnnbinom <- function(x, size, prob, log = FALSE) {
 #' pnnbinom(x2, size = c(2,2), prob = c(.3 ,.6))
 #' @export
 
-pnnbinom <- function(q, size, prob, lower.tail = TRUE, log.p = FALSE) {
+pnnbinom <- function(q, size, prob, lower.tail = TRUE) {
   if (any(size < 1) || any(prob <= 0) || any(prob > 1) || any(q < 0))
     stop(message = "Arguments are incompatible.")
   if (any(q < 0) && any(q - floor(q) == 0))
@@ -82,8 +81,10 @@ pnnbinom <- function(q, size, prob, lower.tail = TRUE, log.p = FALSE) {
   }
   q <- matrix(q, ncol = 2)
 
-  cdf <- stats::pnbinom(q, size = size, prob = prob, lower.tail = lower.tail, log.p =log.p)
+  cdf <- stats::pnbinom(q, size = size, prob = prob)
 
+  if (!lower.tail)
+    cdf <- 1 - cdf
 
   cdf <- matrix(cdf, ncol = 2, byrow = TRUE)
 
@@ -92,14 +93,14 @@ pnnbinom <- function(q, size, prob, lower.tail = TRUE, log.p = FALSE) {
 
   return(cdf)
 }
-#' @name NNBIND
+#' @name Neutrosophic Negative Binomial
 #' @examples
 #' q1 <- seq(0.1, 1, length.out = 40)
 #' qnnbinom(q1, size = 2, prob= 0.5)
 #' q2 <- matrix(seq(0.1, 1, length.out = 40), ncol = 2)
 #' qnnbinom(q2, lambda = c(2, 2))
 #' @export
-qnnbinom <- function(p, size, prob, lower.tail = TRUE, log.p = FALSE) {
+qnnbinom <- function(p, size, prob) {
   if (any(p < 0) || any(p > 1))
     stop(message = "Warning: p should be in the interval [0,1].")
 
@@ -112,7 +113,7 @@ qnnbinom <- function(p, size, prob, lower.tail = TRUE, log.p = FALSE) {
 
   quantiles <- matrix(data = NA, nrow = nrow(p), ncol = 2)
   for (i in 1:ncol(p)) {
-    quantiles[, i] <- stats::qnbinom(p[, i], size = size[i], prob = prob[i], lower.tail = lower.tail, log.p =log.p)
+    quantiles[, i] <- stats::qnbinom(p[, i], size = size[i], prob = prob[i])
   }
 
   swap_rows <- quantiles[, 1] > quantiles[, 2]
@@ -121,7 +122,7 @@ qnnbinom <- function(p, size, prob, lower.tail = TRUE, log.p = FALSE) {
   return(quantiles)
 }
 
-#' @name NNBIND
+#' @name Neutrosophic Negative Binomial
 #' @examples
 #' n <- 10
 #' rnnbinom(n, size = 2, prob= 0.5)
