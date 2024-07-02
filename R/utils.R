12a8928a144e1da215b1795bc513f7b609de320e
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2024-07-02 13:54:50 +0330

    change inv_erf

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,9 @@
 ### error function
-erf <- function(x) 2 * stats::pnorm(x * sqrt(2)) - 1
+erf <- function(x) {
+  2 * stats::pnorm(x * sqrt(2)) - 1
+}
 
 ### inverses error function
-erfinv <- function (x) qnorm((1 + x)/2)/sqrt(2)
+inv_erf <- function (x) {
+  stats::qnorm((1 + x)/2)/sqrt(2)
+}
